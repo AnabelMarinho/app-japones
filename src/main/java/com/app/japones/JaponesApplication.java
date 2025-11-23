2472b0d3b9167e6b295f085fabe06a27942e921b
AnabelMarinho <anabel.soares2001@gmail.com>
2025-11-23 18:49:30 -0300

    inicializando projeto com spring initializr

---
+++ src/main/java/com/app/japones/JaponesApplication.java
@@ -0,0 +1,13 @@
+package com.app.japones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaponesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaponesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/japones/JaponesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.japones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaponesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
